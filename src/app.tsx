f8dc647d7ea00da713640f50a1c9869fd7660924
crispache <cristinaglezpache06@gmail.com>
2023-11-20 14:00:19 +0000

    Enganchar rutas

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppRouter } from "./router";
 import { AppLayout } from "./layout";
 
 export const App = () => {
   return (
     <AppLayout>
-      <p> contenido .... </p>
+       <AppRouter />
     </AppLayout>
   );
 };
